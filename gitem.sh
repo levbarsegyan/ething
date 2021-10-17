707cce9a753bbe080b427fa0d72c2b7cd2de0602
Lev <lev.barsegyan@devit-team.com>
2021-10-17 06:36:20 +0000

    ProjectsInitialised Mon 30 Jul 14:43:25 EEST 2018

--- gitem.sh
+++ gitem.sh
@@ -3,12 +3,13 @@ set -u
 set -e
 export MSG="$1";
 timestamp="$(date)"
+echo "$MSG $timestamp" > MSG;
+git add --all && git commit -m "$MSG $timestamp" && git push;
 for f in bones flesh generator-flesh spider thing
 do
   cd $f;
   echo "=================== $f ==================="
   echo "$MSG $timestamp" > MSG;
-  git add --all && git commit -m "$MSG $timestamp" && git push;
   echo "$f committed"
   cd ..;
 done
